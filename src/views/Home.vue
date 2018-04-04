d186d8c823bd212001695e9de50458f5cfe05a92
Jeff Schenck <jmschenck@gmail.com>
2018-04-04 09:23:54 -0700

    Add bulma so things aren't too ugly

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div>
+    <section class="hero is-dark is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1">Packages</h1>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
