a9b44cf1df749358cd9d5d1ed853e7442f5ef9f4
Jeff Schenck <jmschenck@gmail.com>
2018-04-04 09:19:35 -0700

    Update with basic routing

--- src/router.js
+++ src/router.js
@@ -1,21 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Home from '@/views/Home'
+import Platform from '@/views/Platform'
+import Package from '@/views/Package'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
-  ]
+      path: '/:platform',
+      name: 'platform',
+      component: Platform,
+    },
+    {
+      path: '/:platform/:name',
+      name: 'package',
+      component: Package,
+    },
+  ],
 })
