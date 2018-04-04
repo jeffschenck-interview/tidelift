d43335e864a07e41e319561eac87e7e46ee49b0e
Jeff Schenck <jmschenck@gmail.com>
2018-04-04 10:09:54 -0700

    Display basic version and project info

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
+import VueMoment from 'vue-moment'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(VueMoment)
+
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,18 +1,50 @@
 <template>
-  <div>
+  <div v-if="projectData">
     <section class="hero is-dark is-bold">
       <div class="hero-body">
         <div class="container">
           <h1 class="title is-1">{{ projectData.name }}</h1>
-          <p class="subtitle">{{ projectData.platform }}</p>
+          <p class="subtitle">{{ projectData.description }}</p>
+          <div class="field is-grouped is-grouped-multiline">
+            <div class="control">
+              <div class="tags has-addons">
+                <span class="tag">Platform</span>
+                <span class="tag is-primary">{{ projectData.platform }}</span>
+              </div>
+            </div>
+            <div class="control">
+              <div v-for="license in projectData.normalized_licenses" :key="license" class="tags has-addons">
+                <span class="tag">License</span>
+                <span class="tag is-primary">{{ license }}</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
     <section class="section">
       <div class="container">
-        <p>
-          {{ projectData }}
-        </p>
+        <div class="columns">
+          <div class="column is-8">
+            <h2 class="title is-3">Dependencies</h2>
+          </div>
+          <div class="column">
+            <table class="table is-hoverable is-fullwidth">
+              <thead>
+                <tr>
+                  <th>Version</th>
+                  <th>Released</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="{number, published_at} in projectData.versions" :key="number">
+                  <td>{{ number }}</td>
+                  <td>{{ published_at|moment('MMM D, YYYY') }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -32,7 +64,7 @@ export default {
       return this.$route.params.project
     },
     projectData () {
-      return this.platforms[this.platform][this.project]
+      return this.platforms[this.platform] && this.platforms[this.platform][this.project]
     },
   },
   watch: {
